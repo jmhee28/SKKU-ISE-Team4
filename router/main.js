970e471169981a3eb0c82f4afda754d0c25c44e3
cardioman <cardioman71@gmail.com>
2022-11-17 22:49:04 +0900

    updating keywords array for crawling

--- router/main.js
+++ router/main.js
@@ -1,20 +1,46 @@
 import { parsing } from "../crawling.js"
-import {authorize, addEvents} from "../googleCalender.js"
-let keywords =  ["장학", "취업", "학사"];
+import { authorize, addEvents } from "../googleCalender.js"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { collection, getDocs, query, where } from "firebase/firestore"
 
-async function getCrawled(){
-    let crawlresult =  await parsing(keywords[0]);
+const firebaseConfig = {
+    apiKey: "AIzaSyAEGuIFzQ6MHzVBiq6Q0IgEypC_GwM4eEA",
+    authDomain: "ise-team4.firebaseapp.com",
+    projectId: "ise-team4",
+    storageBucket: "ise-team4.appspot.com",
+    messagingSenderId: "526463390715",
+    appId: "1:526463390715:web:8dbf9d0a817049fe89715e",
+    measurementId: "G-BT0FWC8KQV"
+}
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+let keywords = ["장학", "취업", "학사"];
+let email = "email"
+let skkuid = "skkuid"
+
+async function getCrawled() {
+    const docsref = collection(db, "accounts");
+    const q = query(docsref, where("skkuid", "==", skkuid));
+    const querySnapshot = await getDocs(q);
+    keywords = querySnapshot.docs[0].data().keywords;
+    console.log(keywords);
+    let crawlresult = await parsing(keywords[0]);
     return crawlresult;
-   }
+}
 
 const m = function (app, fs) {
-    let email = "email"
-    let skkuid = "skkuid"
 
     app.get("/", function (req, res) {
         res.render("index")
     });
-    app.get("/keywords", function(req, res){
+    app.get("/keywords",async function (req, res) {
+        const docsref = collection(db, "accounts");
+        const q = query(docsref, where("skkuid", "==", skkuid));
+        const querySnapshot = await getDocs(q);
+        keywords = querySnapshot.docs[0].data().keywords;
         return res.status(200).json({ keywords: keywords });
     })
     app.get("/calendar", function (req, res) {
@@ -26,8 +52,8 @@ const m = function (app, fs) {
         //     console.log("cr: \n" + crawlresult);
         //     res.render("calendar",{ email: email, keywords: keywords,  crawlresult: crawlresult});
         // })
-        
-        res.render("calendar",{ email: email, keywords: keywords, skkuid: skkuid});
+
+        res.render("calendar", { email: email, keywords: keywords, skkuid: skkuid });
     });
 
     app.post("/calendar", function (req, res) {
@@ -39,8 +65,8 @@ const m = function (app, fs) {
         console.log("get register")
         res.render("register")
     });
-    
-    app.post("/login",(req,res) => {  
+
+    app.post("/login", (req, res) => {
         email = req.body.googleid  // 구글캘린더 불러올 때 필요   
         keywords = req.body.keywords
         skkuid = req.body.skkuid
